5c2ff07a725297881407e130d35697e058349ac6
yuhaoyang <haoyuyang@yangqianguan.com>
2023-07-05 18:20:42 +0800

    wip

---
+++ app/(blank)/me/page.tsx
@@ -0,0 +1,3 @@
+export default function a() {
+    return <div className="btn btn-primary w-64">me</div>
+};
--- app/(hasLayout)/layout.tsx
+++ app/(hasLayout)/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '../globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <section>测试</section>
+        {children}
+      </body>
     </html>
   )
 }
