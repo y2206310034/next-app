f9f67a5f8a8a6733ad591612cb9099304e406e06
yuhaoyang <haoyuyang@yangqianguan.com>
2023-07-10 19:07:34 +0800

    wip

--- app/(blank)/me/page.tsx
+++ app/(blank)/me/page.tsx
@@ -1,3 +1,88 @@
-export default function a() {
-    return <div className="btn btn-primary w-64">me</div>
+'use client';
+import {useState} from 'react';
+import { useForm, SubmitHandler } from "react-hook-form"
+import { Input } from "@/components/ui/input"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+
+
+export default function TryDialog() {
+    const [open, setOpen] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const form = useForm({
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    const onSubmit = data => {
+        console.log(data)
+        setOpen(false);
+    }
+
+
+    return <div>
+        <Button onClick={() => setOpen(true)}>Open</Button>
+        <Dialog
+            open={open}
+            onOpenChange={setOpen}
+        >
+            <DialogContent forceMount={true}>
+                <DialogHeader>
+                    <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+                    <DialogDescription>
+                        This action cannot be undone. This will permanently delete your account
+                        and remove your data from our servers.
+                    </DialogDescription>
+                </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="shadcn" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is your public display name.
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <DialogFooter>
+                            <Button type="submit" >Save changes</Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
+            </DialogContent>
+        </Dialog>
+    </div>
 };
--- app/(hasLayout)/a/page.tsx
+++ app/(hasLayout)/a/page.tsx
@@ -1,3 +1,178 @@
+"use client"
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Form, Field, FormSpy } from 'react-final-form'
+import { Button } from "@/components/ui/button"
+
 export default function a() {
-    return <div className="btn btn-primary w-64">对劲吗</div>
+    const onSubmit = values => {
+        console.log(values);
+        console.log('>>>>>');
+    }
+    return (<Form
+        onSubmit={onSubmit}
+        initialValues={{ stooge: 'curly', employed: false }}
+        render={({ handleSubmit, form, submitting, pristine, values }) => (
+            <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <label>First Name</label>
+                    <Field
+                        name="firstName"
+                        component="input"
+                        type="text"
+                        placeholder="First Name"
+                    />
+                </div>
+                <div>
+                <label>Last Name</label>
+                <Field
+                    name="lastName"
+                    component="input"
+                    type="text"
+                    placeholder="Last Name"
+                />
+                </div>
+                <div>
+                <label>Employed</label>
+                    <Field name="employed" component="input" type="checkbox" />
+                </div>
+                <div>
+                <label>Favorite Color</label>
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn m-1">
+                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+                    </label>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                    </ul>
+                </div>
+                <Field name="favoriteColor" component="select">
+                    <option />
+                    <option value="#ff0000">❤️ Red</option>
+                    <option value="#00ff00">💚 Green</option>
+                    <option value="#0000ff">💙 Blue</option>
+                </Field>
+                </div>
+                <div>
+                <label>Toppings</label>
+                <Field name="toppings" component="select" multiple>
+                    <option value="chicken">🐓 Chicken</option>
+                    <option value="ham">🐷 Ham</option>
+                    <option value="mushrooms">🍄 Mushrooms</option>
+                    <option value="cheese">🧀 Cheese</option>
+                    <option value="tuna">🐟 Tuna</option>
+                    <option value="pineapple">🍍 Pineapple</option>
+                </Field>
+                </div>
+                <div>
+                <label>Sauces</label>
+                <div>
+                    <label>
+                    <Field
+                        name="sauces"
+                        component="input"
+                        type="checkbox"
+                        value="ketchup"
+                    />{' '}
+                    Ketchup
+                    </label>
+                    <label>
+                    <Field
+                        name="sauces"
+                        component="input"
+                        type="checkbox"
+                        value="mustard"
+                    />{' '}
+                    Mustard
+                    </label>
+                    <label>
+                    <Field
+                        name="sauces"
+                        component="input"
+                        type="checkbox"
+                        value="mayonnaise"
+                    />{' '}
+                    Mayonnaise
+                    </label>
+                    <label>
+                    <Field
+                        name="sauces"
+                        component="input"
+                        type="checkbox"
+                        value="guacamole"
+                    />{' '}
+                    Guacamole 🥑
+                    </label>
+                </div>
+                </div>
+                <div>
+                <label>Best Stooge</label>
+                <div>
+                    <label>
+                        <Field
+                            name="stooge"
+                            type="radio"
+                            value="32"
+                        >
+                            {props => (
+                                <input
+                                    {...props.input}
+                                    className="radio checked:bg-red-500" />
+                            )}
+                        </Field>
+                        Red pill
+                    </label>
+                    <label>
+                        <Field
+                            name="stooge"
+                            type="radio"
+                            value="moe"
+                        >
+                            {props => (
+                                <input
+                                    {...props.input}
+                                    className="radio checked:bg-red-500"
+                                />
+                            )}
+                        </Field>
+                        Moe
+                    </label>
+                    <label>
+                        <Field
+                            name="stooge"
+                            type="radio"
+                            value="curly"
+                        >
+                            {props => (<input
+                                    {...props.input}
+                                    className="radio checked:bg-red-500"
+                                />
+                            )}
+                        </Field>
+                        <span className="label-text">Curly</span> 
+                    </label>
+                </div>
+                </div>
+                <div>
+                <label>Notes</label>
+                <Field name="notes" component="textarea" placeholder="Notes" />
+                </div>
+                <div className="buttons">
+                    <Button
+                        disabled={submitting}
+                    >
+                        Submit
+                    </Button>
+                    <Button
+                        onClick={form.reset}
+                        disabled={submitting}
+                    >
+                        Reset
+                    </Button>
+                </div>
+            </form>
+            )}
+    />)
 };
